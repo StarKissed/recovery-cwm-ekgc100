7c8939d3feb0b6256f288198d66c604a09ef6005
LoungeKatt <TwistedUmbrella@gmail.com>
2013-08-01 11:02:37 -0400

    Publish all files when compiling

--- buildRecovery.sh
+++ buildRecovery.sh
@@ -4,7 +4,7 @@
 
 # This script is designed by Twisted Playground for use on MacOSX 10.7 but can be modified for other distributions of Mac and Linux
 
-PROPER=`echo $2 | sed 's/\([a-z]\)\([a-zA-Z0-9]*\)/\u\1\2/g'`
+PROPER=`echo $1 | sed 's/\([a-z]\)\([a-zA-Z0-9]*\)/\u\1\2/g'`
 
 HANDLE=TwistedZero
 KERNELSPEC=/Volumes/android/ek-gc100_recovery
@@ -34,62 +34,62 @@ make -j$CPU_JOB_NUM ARCH=arm CROSS_COMPILE=$TOOLCHAIN_PREFIX
 
 if [ -e arch/arm/boot/zImage ]; then
 
-if [ `find . -name "*.ko" | grep -c ko` > 0 ]; then
+    if [ `find . -name "*.ko" | grep -c ko` > 0 ]; then
 
-find . -name "*.ko" | xargs ${TOOLCHAIN_PREFIX}strip --strip-unneeded
+        find . -name "*.ko" | xargs ${TOOLCHAIN_PREFIX}strip --strip-unneeded
 
-if [ ! -d $MODULEOUT ]; then
-mkdir $MODULEOUT
-fi
-if [ ! -d $MODULEOUT/lib ]; then
-mkdir $MODULEOUT/lib
-fi
-if [ ! -d $MODULEOUT/lib/modules ]; then
-mkdir $MODULEOUT/lib/modules
-else
-rm -r $MODULEOUT/lib/modules
-mkdir $MODULEOUT/lib/modules
-fi
+        if [ ! -d $MODULEOUT ]; then
+            mkdir $MODULEOUT
+        fi
+        if [ ! -d $MODULEOUT/lib ]; then
+            mkdir $MODULEOUT/lib
+        fi
+        if [ ! -d $MODULEOUT/lib/modules ]; then
+            mkdir $MODULEOUT/lib/modules
+        else
+            rm -r $MODULEOUT/lib/modules
+            mkdir $MODULEOUT/lib/modules
+        fi
 
-for j in $(find . -name "*.ko"); do
-cp -R "${j}" $MODULEOUT/lib/modules
-done
+        for j in $(find . -name "*.ko"); do
+            cp -R "${j}" $MODULEOUT/lib/modules
+        done
 
-fi
+    fi
 
-cp -R arch/arm/boot/zImage buildimg
+    cp -R arch/arm/boot/zImage buildimg
 
-cd buildimg
-./img.sh
+    cd buildimg
+    ./img.sh
 
-echo "building recovery package"
-cp -R recovery.img ../output
-cd ../
+    echo "building recovery package"
+    cp -R recovery.img ../output
+    cd ../
 
-if [ -e output/recovery.tar ]; then
-rm -R output/recovery.tar
-fi
-if [ -e output/recovery.tar ]; then
-rm -R output/recovery.tar.md5
-fi
-if [ -e output/recovery.tar ]; then
-rm -R output/recovery.tar.md5.gz
-fi
+    if [ -e output/recovery.tar ]; then
+        rm -R output/recovery.tar
+    fi
+    if [ -e output/recovery.tar ]; then
+        rm -R output/recovery.tar.md5
+    fi
+    if [ -e output/recovery.tar ]; then
+        rm -R output/recovery.tar.md5.gz
+    fi
 
-IMAGEFILE=recovery.$PUNCHCARD.img
-KERNELFILE=recovery.$PUNCHCARD.tar
+    IMAGEFILE=recovery.$PUNCHCARD.img
+    KERNELFILE=recovery.$PUNCHCARD.tar
 
-cp -r  output/recovery.img $KERNELREPO/gooserver/$IMAGEFILE
-scp -P 2222 $KERNELREPO/gooserver/$IMAGEFILE $GOOSERVER/galaxycam
+    cp -r  output/recovery.img $KERNELREPO/gooserver/$IMAGEFILE
+    scp -P 2222 $KERNELREPO/gooserver/$IMAGEFILE $GOOSERVER/galaxycam
 
-if cat /etc/issue | grep Ubuntu; then
-    tar -H ustar -c output/recovery.img > output/recovery.tar
-else
-    gnutar -H ustar -c output/recovery.img > output/recovery.tar
-fi
-# Create an md5 kernel image
-if [ "$1" == "1" ]; then
-    KERNELFILE=$KERNELFILE.md5.gz
+    if cat /etc/issue | grep Ubuntu; then
+        tar -H ustar -c output/recovery.img > output/recovery.tar
+    else
+        gnutar -H ustar -c output/recovery.img > output/recovery.tar
+    fi
+    cp -r output/recovery.tar $KERNELREPO/camera/recovery.tar
+    scp -P 2222 $KERNELREPO/gooserver/$KERNELFILE $GOOSERVER/galaxycam
+    rm -R $KERNELREPO/gooserver/$KERNELFILE
     cp -r output/recovery.tar output/recovery.tar.md5
     if cat /etc/issue | grep Ubuntu; then
         md5sum -r output/recovery.tar.md5 >> output/recovery.tar.md5
@@ -97,14 +97,10 @@ if [ "$1" == "1" ]; then
         md5 -r output/recovery.tar.md5 >> output/recovery.tar.md5
     fi
     gzip output/recovery.tar.md5 -c -v > output/recovery.tar.md5.gz
-    cp -r output/recovery.tar.md5.gz $KERNELREPO/recovery.tar.md5.gz
-    cp -r $KERNELREPO/recovery.tar.md5.gz $KERNELREPO/gooserver/$KERNELFILE
-# Skip md5 hash generation
-else
-    cp -r output/recovery.tar $KERNELREPO/recovery.tar
-    cp -r $KERNELREPO/recovery.tar $KERNELREPO/gooserver/$KERNELFILE
+    cp -r output/recovery.tar.md5.gz $KERNELREPO/camera/recovery.tar.md5.gz
+    cp -r $KERNELREPO/camera/recovery.tar.md5.gz $KERNELREPO/gooserver/$KERNELFILE.md5.gz
+    scp -P 2222 $KERNELREPO/gooserver/$KERNELFILE.md5.gz $GOOSERVER/galaxycam
+    rm -R $KERNELREPO/gooserver/$KERNELFILE.md5.gz
 fi
-scp -P 2222 $KERNELREPO/gooserver/$KERNELFILE $GOOSERVER/galaxycam
-rm -R $KERNELREPO/gooserver/$KERNELFILE
 
 cd $KERNELSPEC
